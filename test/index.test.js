116e37fcc3a76527838088898dad0a88b7d1214e
Paul Marrapese <paul.marrapese@gmail.com>
2015-06-05 10:54:52 -0700

    parse float from currency string

--- test/index.test.js
+++ test/index.test.js
@@ -18,5 +18,35 @@ describe('Money', function() {
       Money.localize(10.99, 'fr_FR').should.equal('10,99€');
       Money.localize(1010.99, 'fr_FR').should.equal('1.010,99€');
     });
+
+    it('should parse float from en_US', function() {
+      let money = new Money('en_US');
+      money.parse('$1,234').should.equal(1234);
+      money.parse('$1,234.01').should.equal(1234.01);
+      money.parse('$12.50').should.equal(12.5);
+      money.parse('$12').should.equal(12);
+      money.parse('$.50').should.equal(.50);
+      money.parse('$0.05').should.equal(.05);
+    });
+
+    it('should parse float from en_GB', function() {
+      let money = new Money('en_GB');
+      money.parse('£1,234').should.equal(1234);
+      money.parse('£1,234.01').should.equal(1234.01);
+      money.parse('£12.50').should.equal(12.5);
+      money.parse('£12').should.equal(12);
+      money.parse('£.50').should.equal(.50);
+      money.parse('£0.05').should.equal(.05);
+    });
+
+    it('should parse float from fr_FR', function() {
+      let money = new Money('fr_FR');
+      money.parse('1.234€').should.equal(1234);
+      money.parse('1.234,01€').should.equal(1234.01);
+      money.parse('12,50€').should.equal(12.5);
+      money.parse('12€').should.equal(12);
+      money.parse(',50€').should.equal(.50);
+      money.parse('0,05€').should.equal(.05);
+    });
   });
 });
